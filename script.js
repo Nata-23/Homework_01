4539d81492f81feacc0f149c5f36ead7efefcecf
Nataliia <d_tasha@ukr.net>
2022-02-06 20:46:59 +0200

    added script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// 1
+let userName = prompt("Введите Ваше имя", "");
+alert(userName);
+
+//2
+let varFirst = 10;
+let varSecond = 2;
+
+let sum = varFirst + varSecond;
+alert(sum);
+
+let minus = varFirst - varSecond;
+alert(minus);
+
+let multuplay = varFirst * varSecond;
+alert(multuplay);
+
+let division = varFirst / varSecond;
+alert(division);
+
+let degree = varFirst ** varSecond;
+alert(degree);
+
+//2 c prompt
+/*
+let varFirst, varSecond, sum, minus, multuplay, division, degree;
+
+varFirst = +prompt("Введите первую переменную", "");
+varSecond = +prompt("Введите первую переменную", "");
+
+sum = varFirst + varSecond;
+alert(sum);
+
+minus = varFirst - varSecond;
+alert(minus);
+
+multuplay = varFirst * varSecond;
+alert(multuplay);
+
+division = varFirst / varSecond;
+alert(division);
+
+degree = varFirst ** varSecond;
+alert(degree);*/
